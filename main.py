922fd31cd405dfaccc4f33502155c453f836e6c0
karsofya <karsofya@gmail.com>
2024-11-16 17:10:41 +0200

    Project initial setup; add requirements

---
+++ main.py
@@ -0,0 +1,17 @@
+from textblob import Word
+import re
+
+def check_word_spelling(word):
+
+    word = Word(word)
+
+    result = word.spellcheck()
+
+    if word == result[0][0]:
+        print(f'Spelling of "{word}" is correct!')
+    else:
+        print(f'Spelling of "{word}" is not correct!')    
+        print(f'correct spelling of "{word}": "{result[0][0]}" (with {round(result[0][1], 2)} confidence)')
+
+if __name__ == "__main__":
+   check_word_spelling(input("Enter word: "))
