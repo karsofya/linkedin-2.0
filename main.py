ad27c24ade418dc443a96f7519a9bcf428a1b279
karsofya <karsofya@gmail.com>
2024-11-16 18:01:26 +0200

    text check added

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from textblob import Word
-import re
+from textblob import Word, TextBlob
 
-def check_word_spelling(word):
+def check_word_spelling(word: str):
 
-    word = Word(word)
+    word_checker = Word(word)
+    result = word_checker.spellcheck()
+    print(result)
 
-    result = word.spellcheck()
-
-    if word == result[0][0]:
+    if word == result[0][0] and result[0][1] == 1:
         print(f'Spelling of "{word}" is correct!')
     else:
         print(f'Spelling of "{word}" is not correct!')    
         print(f'correct spelling of "{word}": "{result[0][0]}" (with {round(result[0][1], 2)} confidence)')
 
+def check_text_spelling(text: str):
+    text_checker = TextBlob(text)
+    print (text_checker)
+    print(text_checker.correct()) 
+
+
 if __name__ == "__main__":
    print("Starting spelling check..")
-   check_word_spelling(input("Enter word: "))
+   #check_word_spelling(input("Enter word: "))
+   check_text_spelling(input("Enter text: "))
